08f67c567cb076634fe5a774c67b00b19e483de3
Diyan D <122938689+DidkoD2@users.noreply.github.com>
2023-01-22 23:06:53 +0200

    Added playermove

---
+++ RockPaperScisoors.cs
@@ -0,0 +1,30 @@
+﻿
+const string rock = "Rock";
+const string paper = "Paper";
+const string scissors = "Scissors";
+
+Console.WriteLine("Choose [r]ock, [p]aper, [s]cissors:");
+string playerMove = Console.ReadLine();
+
+if (playerMove == "r" || playerMove == "rock")
+{
+    playerMove = rock;
+}
+else if (playerMove == "p" || playerMove == "paper")
+{
+    playerMove = paper;
+}
+else if (playerMove == "s" || playerMove == "scissors")
+{
+    playerMove = scissors;
+}
+else
+{
+    Console.WriteLine("Invalid Input. Try Again...");
+}
+
+
+
+
+
+
